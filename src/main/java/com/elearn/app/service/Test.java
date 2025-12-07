60ae3be2578902360448ec02c8513d761fbfe03e
priyesh kumar <59879071+priyeshsingh535@users.noreply.github.com>
2025-12-07 21:49:12 +0530

    adding a simple test

---
+++ src/main/java/com/elearn/app/service/Test.java
@@ -0,0 +1,9 @@
+package com.elearn.app.service;
+
+import java.sql.SQLOutput;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is only for testing the git");
+    }
+}
